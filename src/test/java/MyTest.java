f3e3437e496dd2159c257e63b9fe879ad8cec983
borisinio <45246582+borisinio@users.noreply.github.com>
2021-02-17 12:20:41 +0200

    Add files via upload

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,9 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results/ManualRunnerTest_OctaneGherkinResults.xml",
+        features = "src/test/resources/F500/", tags = "not @exclude")
+public class MyTest {
+}
