5a08696b945f34d8589cda11035a8a459365b22e
borisinio <45246582+borisinio@users.noreply.github.com>
2021-02-17 12:20:10 +0200

    Delete MyTest.java

--- src/test/java/MyTest.java
+++
@@ -1,9 +0,0 @@
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = "com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results/ManualRunnerTest_OctaneGherkinResults.xml",
-        features = "src/test/resources/testing/", tags = "not @exclude")
-public class MyTest {
-}
